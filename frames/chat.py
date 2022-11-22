50979d8f06e7639f9b15e7cd202f65c05fc8116e
ellatechlearning <ellatechlearning@gmail.com>
2022-11-22 10:28:31 +0100

    init commit

---
+++ frames/chat.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import ttk
+import requests
+from frames.message_window import MessageWindow
+
+messages = [{"message": "Hi, Do you need help?", "date": 15498487}]
+message_labels = []
+
+
+class Chat(ttk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.messages_window = MessageWindow(self)
+        self.messages_window.grid(row=0, column=0, sticky="NSEW", pady=5)
+
+        input_frame = ttk.Frame(self, padding=10)
+        input_frame.grid(row=1, column=0, sticky="EW")
+
+        message_fetch = ttk.Button(
+            input_frame,
+            text="Fetch",
+            command=self.get_messages
+        )
+        message_fetch.pack()
+        self.messages_window.update_message_widgets()
+
+    def get_messages(self):
+        global messages
+        messages = requests.get("http://167.99.63.70/messages").json()
+        self.messages_window.update_message_widgets()
+    
+    
